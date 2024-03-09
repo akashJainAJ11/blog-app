dac7f1b2eff649e1b77cff69da536c0f26913c53
akashJainAJ11 <akashjain100200@gmail.com>
2024-03-09 20:18:35 +0530

    backed modified

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -98,3 +98,62 @@ blogRouter.put('/', async (c) => {
     return c.text('updated post');
 })
 
+
+blogRouter.get('/bulk', async (c) => {
+    const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL	,
+	}).$extends(withAccelerate());
+
+    const posts = await prisma.post.findMany({
+        select: {
+            id: true,
+            title: true,
+            content: true,
+            author: {
+                select: {
+                    username: true
+                }
+            }
+        }
+    });
+    return c.json({
+		posts
+	})
+})
+
+
+blogRouter.get('/:id', async (c) => {
+	const id = c.req.param("id");
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL	,
+	}).$extends(withAccelerate());
+
+	try {
+		const post = await prisma.post.findFirst({
+			where: {
+				id: Number(id)
+			},
+			select: {
+				id: true,
+                title: true,
+                content: true,
+                author: {
+                    select: {
+                        username: true
+                    }
+                }
+            }
+		});
+		return c.json({
+			post
+		});
+	} catch (e) {
+		c.status(411);
+		return c.json({
+			message: "Error while fecting blog post"
+		})
+	}
+	
+})
+
+
